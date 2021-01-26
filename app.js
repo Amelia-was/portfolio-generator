7b7de8f04677d19938d9ad71bddfe3afe635995e
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-01-26 10:46:32 -0500

    use inquirer to capture user input

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // use require() to include specific modules
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// const [name, github] = profileDataArgs;
 
 // create HTML file with captured data
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+// 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
